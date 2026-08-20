51c94d9756715fa8edc7b1760f9b6a7777cc7fc5
daizzt10@gmail.com <daizzt10@gmail.com>
2026-08-20 08:02:07 +0700

    add

---
+++ session20/models/classroom.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+
+from database.base import Base
+
+
+class Classroom(Base):
+    __tablename__ = "classroom"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    class_code = Column(String(10), unique=True, nullable=False)
+    class_name = Column(String(100), nullable=False)
+
+    students = relationship(
+        "Student",
+        back_populates="classroom"
+    )
---
+++ session20/models/student.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+
+from database.base import Base
+
+
+class Student(Base):
+    __tablename__ = "student"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    student_code = Column(String(10), unique=False, nullable=False)
+    full_name = Column(String(100), nullable=False)
+    email = Column(String(100), nullable=False, unique=False)
+    class_id = Column(Integer, ForeignKey("classroom.id"), nullable=False)
+
+    classroom = relationship("Classroom", back_populates="students")
